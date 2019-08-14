16784f9971e38b2b5c23eb2517c90fb10ed01243
wanggang <gang_wang1@asdc.com.cn>
2019-08-14 10:55:09 +0800

    Attendanceview Item and List

--- app/Repositories/EloquentAttendanceviewRepository.php
+++ app/Repositories/EloquentAttendanceviewRepository.php
@@ -10,18 +10,29 @@ use App\Repositories\Contracts\AttendanceviewRepository;
 class EloquentAttendanceviewRepository extends AbstractEloquentRepository implements AttendanceviewRepository
 {
 
-	/**
-     * @inheritdoc
-     */
-    public function findBy(array $searchCriteria = [], $operatorCriteria = []) {
-        return parent::findBy($searchCriteria, $operatorCriteria);
-    }
-
     /**
      * @inheritdoc
      */
-    public function findOne($id) {
-        return parent::findOne($id);
+    public function getAttendanceviewItem(array $searchCriteria = []) {
+        return parent::findOneBy($searchCriteria);
+    }
+
+	/**
+     * @inheritdoc
+     */
+    public function getAttendanceviewList(array $searchCriteria = [], $operatorCriteria = []) {
+        $params = array();
+        if (isset($searchCriteria['uid'])) {
+            $params['uid'] = trim($searchCriteria['uid']);
+        }
+        if (isset($searchCriteria['start_time']) && isset($searchCriteria['end_time'])) {
+            $params['workdate'] = date('Ymd', $searchCriteria['start_time']) . "~" . date('Ymd', $searchCriteria['end_time']);
+            $operatorCriteria['workdate'] = 'between';
+        }
+        $params['orderby'] = isset($searchCriteria['orderby']) ? $searchCriteria['orderby'] : 'checkin_at ASC,checkout_at DESC';
+        $params['page'] = isset($searchCriteria['page']) ? $searchCriteria['page'] : 1;
+        $params['per_page'] = isset($searchCriteria['per_page']) ? $searchCriteria['per_page'] : 15;
+        return parent::findBy($params, $operatorCriteria);
     }
 
 }
