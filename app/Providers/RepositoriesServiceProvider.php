a03db816509db72f934b38d5471102c0f0483f02
w17600101602 <373933639@qq.com>
2019-09-17 10:12:01 +0800

    first push

--- app/Providers/RepositoriesServiceProvider.php
+++ app/Providers/RepositoriesServiceProvider.php
@@ -4,41 +4,11 @@ namespace App\Providers;
 
 use App\Models\User;
 use App\Models\Test;
-use App\Models\Framework;
-use App\Models\Frameworkdetails;
-use App\Models\Supplier;
-use App\Models\Attendance;
-use App\Models\Attendanceview;
-use App\Models\Staff;
-use App\Models\Contractorder;
-use App\Models\Contractorderquota;
-use App\Models\Dept;
-use App\Models\Option;
 use Illuminate\Support\ServiceProvider;
 use App\Repositories\Contracts\UserRepository;
 use App\Repositories\Contracts\TestRepository;
-use App\Repositories\Contracts\FrameworkRepository;
-use App\Repositories\Contracts\FrameworkdetailsRepository;
-use App\Repositories\Contracts\SupplierRepository;
-use App\Repositories\Contracts\AttendanceRepository;
-use App\Repositories\Contracts\AttendanceviewRepository;
-use App\Repositories\Contracts\StaffRepository;
-use App\Repositories\Contracts\ContractorderRepository;
-use App\Repositories\Contracts\ContractorderquotaRepository;
-use App\Repositories\Contracts\DeptRepository;
-use App\Repositories\Contracts\OptionRepository;
 use App\Repositories\EloquentUserRepository;
 use App\Repositories\EloquentTestRepository;
-use App\Repositories\EloquentFrameworkRepository;
-use App\Repositories\EloquentFrameworkdetailsRepository;
-use App\Repositories\EloquentSupplierRepository;
-use App\Repositories\EloquentAttendanceRepository;
-use App\Repositories\EloquentAttendanceviewRepository;
-use App\Repositories\EloquentStaffRepository;
-use App\Repositories\EloquentContractorderRepository;
-use App\Repositories\EloquentContractorderquotaRepository;
-use App\Repositories\EloquentDeptRepository;
-use App\Repositories\EloquentOptionRepository;
 
 class RepositoriesServiceProvider extends ServiceProvider
 {
@@ -62,36 +32,6 @@ class RepositoriesServiceProvider extends ServiceProvider
         $this->app->bind(TestRepository::class, function () {
             return new EloquentTestRepository(new Test());
         });
-        $this->app->bind(FrameworkRepository::class, function () {
-            return new EloquentFrameworkRepository(new Framework());
-        });
-        $this->app->bind(FrameworkdetailsRepository::class, function () {
-            return new EloquentFrameworkdetailsRepository(new Frameworkdetails());
-        });
-        $this->app->bind(SupplierRepository::class, function () {
-            return new EloquentSupplierRepository(new Supplier());
-        });
-        $this->app->bind(AttendanceRepository::class, function () {
-            return new EloquentAttendanceRepository(new Attendance());
-        });
-        $this->app->bind(AttendanceviewRepository::class, function () {
-            return new EloquentAttendanceviewRepository(new Attendanceview());
-        });
-        $this->app->bind(StaffRepository::class, function () {
-            return new EloquentStaffRepository(new Staff());
-        });
-        $this->app->bind(ContractorderRepository::class, function () {
-            return new EloquentContractorderRepository(new Contractorder());
-        });
-        $this->app->bind(ContractorderquotaRepository::class, function () {
-            return new EloquentContractorderquotaRepository(new Contractorderquota());
-        });
-        $this->app->bind(DeptRepository::class, function () {
-            return new EloquentDeptRepository(new Dept());
-        });
-        $this->app->bind(OptionRepository::class, function () {
-            return new EloquentOptionRepository(new Option());
-        });
     }
 
     /**
@@ -104,15 +44,6 @@ class RepositoriesServiceProvider extends ServiceProvider
         return [
             UserRepository::class,
             TestRepository::class,
-            FrameworkRepository::class,
-            FrameworkdetailsRepository::class,
-            SupplierRepository::class,
-            AttendanceRepository::class,
-            AttendanceviewRepository::class,
-            Staff::class,
-            Contractorder::class,
-            Contractorderquota::class,
-            Dept::class
         ];
     }
 }
--- app/Repositories/Contracts/OptionRepository.php
+++
@@ -1,7 +0,0 @@
-<?php //app/Repositories/Contracts/UserRepository.php
-
-namespace App\Repositories\Contracts;
-
-interface OptionRepository extends BaseRepository
-{
-}
